3fffc5abcafb57a03664a3f4704fe4a1d7f5f31c
T-taku <39856622+T-taku@users.noreply.github.com>
2021-02-20 01:27:21 +0900

    [WIP] ⚙️ popup page

--- src/js/rank-scratch.js
+++ src/js/rank-scratch.js
@@ -1,17 +1,17 @@
 function getScratchrank(){
     var request = new XMLHttpRequest();
     request.responseType = "json";
-    var profileDetails = document.querySelector(".profile-details");
+    var footerclass = document.querySelector(".footer");
     var username = document.querySelector(".header-text h2").innerText;
     var apiurl = "https://scratchdb.lefty.one/v2/user/info/" + username;
     request.open("GET", apiurl, true);
     request.onload = function(){
         var rank = this.response["statistics"]["ranks"]["followers"];
-        var child = document.createElement("span");
-        child.innerText = "Scratchのフォロワーランキング(世界中): " + String(rank);
-        child.className = "scratch-rank"
+        var child = document.createElement("div");
+        child.innerText = "Scratchのフォロワーランキング(世界中): " + String(rank) + "位";
+        child.className = "box-rank"
         var locationText = document.querySelector(".location");
-        profileDetails.insertBefore(child, locationText);
+        footerclass.insertBefore(child, locationText);
     }
     request.send();
 };
